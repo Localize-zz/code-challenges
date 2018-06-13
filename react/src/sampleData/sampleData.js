dd2b4b60972e017e9a5e7568f29672c895859890
Chris Wohlers <chriswohlers@gmail.com>
2018-06-12 23:29:46 -0700

    Add sample products data.

---
+++ react/src/sampleData/sampleData.js
@@ -0,0 +1,23 @@
+const products = [{
+  name: 'Mountain Bike',
+  price: 2000,
+  id: 1,
+},
+{
+  name: 'Surf Board',
+  price: 500,
+  id: 2,
+},
+{
+  name: 'Wetsuit',
+  price: 200,
+  id: 3,
+},
+{
+  name: 'Guitar',
+  price: 300,
+  id: 4,
+},
+];
+
+export default products;
