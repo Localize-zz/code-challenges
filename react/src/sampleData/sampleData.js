6e7c862cf47a68f719e99383ea91176007ee8cd3
Chris Wohlers <chriswohlers@gmail.com>
2018-06-13 22:55:49 -0700

    Add new products, like data and image refs.

--- react/src/sampleData/sampleData.js
+++ react/src/sampleData/sampleData.js
@@ -1,23 +1,53 @@
-const products = [{
-  name: 'Mountain Bike',
-  price: 2000,
-  id: 1,
-},
-{
-  name: 'Surf Board',
-  price: 500,
-  id: 2,
-},
-{
-  name: 'Wetsuit',
-  price: 200,
-  id: 3,
-},
-{
-  name: 'Guitar',
-  price: 300,
-  id: 4,
-},
-];
+const products = {
+  1: {
+    name: 'Mountain Bike',
+    price: 2000,
+    id: 1,
+    image: '1.png',
+    likes: 0,
+  },
+  2: {
+    name: 'Surf Board',
+    price: 500,
+    id: 2,
+    image: '2.png',
+    likes: 0,
+  },
+  3: {
+    name: 'Wetsuit',
+    price: 200,
+    id: 3,
+    image: '3.jpg',
+    likes: 0,
+  },
+  4: {
+    name: 'Guitar',
+    price: 300,
+    id: 4,
+    image: '4.png',
+    likes: 0,
+  },
+  5: {
+    name: 'Frisbee',
+    price: 10,
+    id: 5,
+    image: '5.png',
+    likes: 0,
+  },
+  6: {
+    name: 'Grand Piano',
+    price: 12000,
+    id: 6,
+    image: '6.png',
+    likes: 0,
+  },
+  7: {
+    name: 'Volleyball',
+    price: 5,
+    id: 7,
+    image: '7.png',
+    likes: 0,
+  },
+};
 
 export default products;
