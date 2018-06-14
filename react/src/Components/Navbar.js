6f592e433a1368d2274ce906b3c197b4b91ae07b
Chris Wohlers <chriswohlers@gmail.com>
2018-06-13 23:33:21 -0700

    Reformat as single column layout.

--- react/src/Components/Navbar.js
+++ react/src/Components/Navbar.js
@@ -6,15 +6,13 @@ const Navbar = () => (
     <div className="Logo">
       <img src={logo} className="Wribbn-logo" alt="logo" />
     </div>
+    <div className="Menu">
+      <div className="Menu-item"><i className="material-icons">explore</i>Explore</div>
+      <div className="Menu-item"><i className="material-icons">chat_bubble</i>Chat</div>
+      <div className="Menu-item"><i className="material-icons">account_circle</i>Profile</div>
+    </div>
     <div className="Search">
       <input type="text" placeholder="Search..." />
-    </div>
-    <div className="Menu">
-      <ul className="Menu-list">
-        <li className="Nav-item"><i className="material-icons">explore</i>Explore</li>
-        <li className="Nav-item"><i className="material-icons">chat_bubble</i>Chat</li>
-        <li className="Nav-item"><i className="material-icons">account_circle</i>Profile</li>
-      </ul>
     </div>
   </div>
 
